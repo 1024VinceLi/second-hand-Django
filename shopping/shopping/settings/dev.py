a5b405cd6a0e49a1bf8d91ec5360c309c999b692
Vince <15981981193@163.com>
2019-06-28 14:48:54 +0800

    配置文件

--- shopping/shopping/settings/dev.py
+++ shopping/shopping/settings/dev.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+sys.path.insert(0, os.path.join(BASE_DIR, "apps"))
+print(sys.path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -38,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
-    'shopping.apps.users.apps.UsersConfig' # 注册应用
+    # 'shopping.apps.users.apps.UsersConfig' # 注册应用
+    'users.apps.UsersConfig' # 注册应用
+
 ]
 
 MIDDLEWARE = [
