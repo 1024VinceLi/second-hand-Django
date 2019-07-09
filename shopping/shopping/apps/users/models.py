4580c7881656c1a5ffa71857ebbc9b151fd9da44
Vince <15981981193@163.com>
2019-07-09 12:49:09 +0800

    创建用户地址模型类,完成数据库迁移

--- shopping/shopping/apps/users/models.py
+++ shopping/shopping/apps/users/models.py
@@ -5,6 +5,7 @@ from itsdangerous import TimedJSONWebSignatureSerializer as TJWSSerializer
 
 
 # Create your models here.
+from shopping.utils.models import BaseModel
 from users import constants
 
 
@@ -13,6 +14,8 @@ class User(AbstractUser):
                              # 最大长度为11    唯一,不可重复    昵称为 手机号
     mobile = models.CharField(max_length=11, unique=True, verbose_name="手机号")
     email_active = models.BooleanField(default=False, verbose_name='邮箱验证状态')
+    default_address = models.ForeignKey('Address', related_name='users', null=True, blank=True,
+                                        on_delete=models.SET_NULL, verbose_name='默认地址')
 
     class Meta:
         db_table = "tb_users"
@@ -49,3 +52,46 @@ class User(AbstractUser):
                 return None
             else:
                 return user
+
+
+class Address(BaseModel):
+    """
+    用户地址
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='addresses', verbose_name='用户')
+    title = models.CharField(max_length=20, verbose_name='地址名称')
+    receiver = models.CharField(max_length=20, verbose_name='收货人')
+    province = models.ForeignKey('areas.Area', on_delete=models.PROTECT, related_name='province_addresses', verbose_name='省')
+    city = models.ForeignKey('areas.Area', on_delete=models.PROTECT, related_name='city_addresses', verbose_name='市')
+    district = models.ForeignKey('areas.Area', on_delete=models.PROTECT, related_name='district_addresses', verbose_name='区')
+    place = models.CharField(max_length=50, verbose_name='地址')
+    mobile = models.CharField(max_length=11, verbose_name="手机")
+    tel = models.CharField(max_length=20, null=True, blank=True, default='', verbose_name='固定电话')
+    email = models.CharField(max_length=30, null=True, blank=True, default='',verbose_name='电子邮箱')
+    is_delete = models.BooleanField(default=False, verbose_name='逻辑删除')
+
+
+    class Meta:
+        db_table = 'tb_address'
+        verbose_name = "用户地址"
+        verbose_name_plural = verbose_name
+        ordering = ['-update_time']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
