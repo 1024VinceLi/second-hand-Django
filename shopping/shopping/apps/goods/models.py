ca88558264aaa3585a3e74ee5dfbbc22f6185250
Vince <15981981193@163.com>
2019-07-11 18:10:54 +0800

    添加docker配置图片文件存储服务

--- shopping/shopping/apps/goods/models.py
+++ shopping/shopping/apps/goods/models.py
@@ -2,7 +2,8 @@
 
 # Create your models here.
 from django.db import models
-
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 from  shopping.utils.models import BaseModel
 
 class GoodsCategory(BaseModel):
@@ -72,6 +73,13 @@ class Goods(BaseModel):
     category3 = models.ForeignKey(GoodsCategory, on_delete=models.PROTECT, related_name='cat3_goods', verbose_name='三级类别')
     sales = models.IntegerField(default=0, verbose_name='销量')
     comments = models.IntegerField(default=0, verbose_name='评价数')
+    desc_detail = RichTextUploadingField(default='', verbose_name='详细介绍')
+    desc_pack = RichTextField(default='', verbose_name='包装信息')
+    desc_service = RichTextUploadingField(default='', verbose_name='售后服务')
+
+
+
+
 
     class Meta:
         db_table = 'tb_goods'
