4379c7e97171c4d851bbf68f944705200f59a1ac
Vince <15981981193@163.com>
2019-07-11 18:34:44 +0800

    注册contents和goods应用中的模型类到admin中

--- shopping/shopping/apps/goods/admin.py
+++ shopping/shopping/apps/goods/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib import admin
 
+from .models import *
 # Register your models here.
+
+"""
+注册goods应用中的模型类
+"""
+
+admin.site.register(GoodsCategory)
+admin.site.register(GoodsChannel)
+admin.site.register(Goods)
+admin.site.register(Brand)
+admin.site.register(GoodsSpecification)
+admin.site.register(SpecificationOption)
+admin.site.register(SKU)
+admin.site.register(SKUSpecification)
+admin.site.register(SKUImage)
